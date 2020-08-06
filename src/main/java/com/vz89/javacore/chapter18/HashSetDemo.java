6a608c4747755e2d4fba2977229b47827f50f633
Vladimir <vzlobin2003@yandex.ru>
2020-08-06 21:13:28 +0300

    HashSetDemo

---
+++ src/main/java/com/vz89/javacore/chapter18/HashSetDemo.java
@@ -0,0 +1,18 @@
+package main.java.com.vz89.javacore.chapter18;
+
+import java.util.HashSet;
+
+public class HashSetDemo {
+    public static void main(String[] args) {
+        HashSet<String> hs = new HashSet<>();
+        hs.add("Бетта");
+        hs.add("Альфа");
+        hs.add("Эта");
+        hs.add("Гамма");
+        hs.add("Эпсилон");
+        hs.add("Омега");
+
+
+        System.out.println(hs);
+    }
+}
