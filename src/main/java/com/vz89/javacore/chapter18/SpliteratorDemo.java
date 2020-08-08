1592902c661c2881de972f7b2e5af1eb2beef8db
Vladimir <vzlobin2003@yandex.ru>
2020-08-08 11:51:38 +0300

    SpliteratorDemo

---
+++ src/main/java/com/vz89/javacore/chapter18/SpliteratorDemo.java
@@ -0,0 +1,32 @@
+package main.java.com.vz89.javacore.chapter18;
+
+import org.w3c.dom.ls.LSOutput;
+
+import java.util.ArrayList;
+import java.util.Spliterator;
+
+public class SpliteratorDemo {
+    public static void main(String[] args) {
+        ArrayList<Double> vals = new ArrayList<>();
+
+        vals.add(1.0);
+        vals.add(2.0);
+        vals.add(3.0);
+        vals.add(4.0);
+        vals.add(5.0);
+
+        System.out.println("Содержимое списочного массива vals: \n");
+        Spliterator<Double> spliterator = vals.spliterator();
+        while(spliterator.tryAdvance(System.out::println));
+
+        spliterator = vals.spliterator();
+        ArrayList<Double> sqrs = new ArrayList<>();
+        while(spliterator.tryAdvance(n->sqrs.add(Math.sqrt(n))));
+
+        System.out.println("Содержимое списочного массива sqrs: \n");
+        spliterator = sqrs.spliterator();
+        spliterator.forEachRemaining(System.out::println);
+
+
+    }
+}
