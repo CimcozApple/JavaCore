115539c22b52a6bbb5d3bc20a9dfe2efb6cd6651
Vladimir <vzlobin2003@yandex.ru>
2020-08-22 20:27:58 +0300

    throws demo

---
+++ src/main/java/com/vz89/javacore/chapter10/ThrowsDemo.java
@@ -0,0 +1,17 @@
+package main.java.com.vz89.javacore.chapter10;
+
+public class ThrowsDemo {
+    static void throwOne() throws IllegalAccessException {
+        System.out.println("В теле метода throwOne().");
+        throw new IllegalAccessException("Демонстрация");
+    }
+
+    public static void main(String[] args) {
+        try {
+            throwOne();
+        } catch (IllegalAccessException e) {
+            System.out.println("Перехвачено исключение: " + e);
+        }
+    }
+}
+
