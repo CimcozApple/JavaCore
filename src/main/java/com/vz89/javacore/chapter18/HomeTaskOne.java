43935c90545dab35e0f84ca7a8efd29490b3deec
Vladimir <vzlobin2003@yandex.ru>
2020-08-22 09:44:03 +0300

    HomeTaskOne correct

--- src/main/java/com/vz89/javacore/chapter18/HomeTaskOne.java
+++ src/main/java/com/vz89/javacore/chapter18/HomeTaskOne.java
@@ -9,14 +9,14 @@ public class HomeTaskOne {
     public static void main(String[] args) {
 
 
-        System.out.println(Arrays.toString(method1(new Integer[]{3, 8, 15, 17}, 23)));
+        System.out.println(Arrays.toString(findIndexesBySumInArray(new Integer[]{3, 8, 15, 17}, 23)));
 
-        method2(new Integer[]{4, 5, 6, 6, 8});
-        method2(new Integer[]{4, 5, 6, 7, 8});
+        findDublicateInArray(new Integer[]{4, 5, 6, 6, 8});
+        findDublicateInArray(new Integer[]{4, 5, 6, 7, 8});
     }
 
 
-    public static Integer[] method1(Integer[] array, int sum) {
+    public static Integer[] findIndexesBySumInArray(Integer[] array, int sum) {
         int idx = 0;
         Map<Integer, Integer> map = new LinkedHashMap<>();
         for (Integer i : array) {
@@ -30,11 +30,10 @@ public class HomeTaskOne {
         return null;
     }
 
-    public static boolean method2(Integer[] array) {
+    public static boolean findDublicateInArray(Integer[] array) {
         for (Integer i : array) {
             int repeat = Collections.frequency(Arrays.asList(array), i);
             if (repeat > 1) {
-                System.out.println("Число " + i + " повторяется " + repeat);
                 return true;
             }
         }
