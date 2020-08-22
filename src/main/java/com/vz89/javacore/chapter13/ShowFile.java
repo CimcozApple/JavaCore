b84993bd5706066dc4e1ba15e4e4b39fb8a88cd5
Vladimir <vzlobin2003@yandex.ru>
2020-08-22 23:57:33 +0300

    ShowFile

---
+++ src/main/java/com/vz89/javacore/chapter13/ShowFile.java
@@ -0,0 +1,32 @@
+package main.java.com.vz89.javacore.chapter13;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+public class ShowFile {
+    public static void main(String[] args) {
+        int i;
+        FileInputStream fin = null;
+
+        if (args.length != 1) {
+            System.out.println("Использование: ShowFile имя_файла");
+            return;
+        }
+
+        try {
+            fin = new FileInputStream(args[0]);
+            do {
+                i = fin.read();
+                if (i != -1) System.out.println((char) i);
+            } while (i != -1);
+        } catch (IOException e) {
+            System.out.println("Произошла ошибка ввода-вывода" + e);
+        } finally {
+            try {
+                if (fin != null) fin.close();
+            } catch (IOException e) {
+                System.out.println("Ошибка закрытия файла");
+            }
+        }
+    }
+}
