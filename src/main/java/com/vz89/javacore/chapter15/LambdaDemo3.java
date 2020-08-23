68c342f5b253e1c2017af682da03ef357937de57
Vladimir <vzlobin2003@yandex.ru>
2020-08-23 20:41:47 +0300

    BlockLambda

---
+++ src/main/java/com/vz89/javacore/chapter15/LambdaDemo3.java
@@ -0,0 +1,15 @@
+package main.java.com.vz89.javacore.chapter15;
+
+interface NumericTest2 {
+    boolean test(int n, int d);
+}
+
+public class LambdaDemo3 {
+    public static void main(String[] args) {
+        NumericTest2 isFactor = (n, d) -> (n % d) == 0;
+
+        if (isFactor.test(10,2)) System.out.println("Число 2 ялвляется множителем числа 10");
+        if (!isFactor.test(10,3)) System.out.println("Число 3 не ялвляется множителем числа 10");
+    }
+
+}
