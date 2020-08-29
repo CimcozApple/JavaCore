76ba89f863d9799d649d4f739a6c28c8bff33f65
Vladimir <vzlobin2003@yandex.ru>
2020-08-29 17:13:15 +0300

    SequenctInputStreamDemo

---
+++ src/main/java/com/vz89/javacore/chapter20/SequenceInputStreamDemo.java
@@ -0,0 +1,48 @@
+package main.java.com.vz89.javacore.chapter20;
+
+import java.io.*;
+import java.util.Enumeration;
+import java.util.Vector;
+
+class InputStreamEnumerator implements Enumeration<FileInputStream> {
+    private Enumeration<String> files;
+
+    public InputStreamEnumerator(Vector<String> files) {
+        this.files = files.elements();
+    }
+
+    @Override
+    public boolean hasMoreElements() {
+        return files.hasMoreElements();
+    }
+
+    @Override
+    public FileInputStream nextElement() {
+        try {
+            return new FileInputStream(files.nextElement().toString());
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
+
+public class SequenceInputStreamDemo {
+    public static void main(String[] args) {
+        int c;
+        Vector<String> files = new Vector<>();
+
+        files.addElement("file1.txt");
+        files.addElement("file2.txt");
+        files.addElement("file3.txt");
+
+        InputStreamEnumerator ise = new InputStreamEnumerator(files);
+
+        try (InputStream input = new SequenceInputStream(ise);) {
+            while ((c = input.read()) != -1) System.out.print((char) c);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
