fa9457c47ca8feef60614c67a7ce10791a0f2b03
Vladimir <vzlobin2003@yandex.ru>
2020-09-30 22:19:57 +0300

    Interpreter

---
+++ src/main/java/com/vz89/javacore/designPatterns/behavioral/interpreter/Expression.java
@@ -0,0 +1,5 @@
+package main.java.com.vz89.javacore.designPatterns.behavioral.interpreter;
+
+public interface Expression {
+    public boolean interpret(String context);
+}
---
+++ src/main/java/com/vz89/javacore/designPatterns/behavioral/interpreter/TerminalExpression.java
@@ -0,0 +1,14 @@
+package main.java.com.vz89.javacore.designPatterns.behavioral.interpreter;
+
+public class TerminalExpression implements Expression {
+    private String data;
+
+    public TerminalExpression(String data) {
+        this.data = data;
+    }
+
+    @Override
+    public boolean interpret(String context) {
+        return context.contains(data);
+    }
+}
